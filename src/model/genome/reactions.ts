66b889348f930be908bea81f50fe89ec2fa35aa8
Maxim <maxim@oprosso.ru>
2020-03-20 17:43:17 +0300

    remove log

--- src/model/genome/reactions.ts
+++ src/model/genome/reactions.ts
@@ -4,8 +4,8 @@ import { getRandomFreePointAround } from "../helpers";
 
 export class ReactionsRegistry {
   static obstacleMoveRandom(cell: Cell, world: World) : boolean {
-    console.log('omg, its busy here, i ll turn away')
     cell.position = getRandomFreePointAround(cell.position, world);
+    
     return true;
   }
 }
