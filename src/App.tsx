5ed58c90e7dd503931f22c78f7e1045a448398ef
max <maxim181011@mail.ru>
2020-03-22 20:24:14 +0300

    controls

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,25 @@ import { observer } from "mobx-react";
 import { World } from "./model/world";
 
 import "./App.scss";
+import { Genome } from "./model/life/genome";
+import { AliveCell } from "./model/life/cells/aliveCell";
+import { Point } from "./model/point";
+import { InstinctRegistry } from "./model/life/genome/instincts";
+import { getRandomMoveInstinct } from "./model/helpers";
 
-interface IProps {
-  world: World
-}
 
-const App = observer((props: IProps) => {
-  const { world } = props;
+const App = observer(() => {
+  const [world, setWorld] = useState<World>(newWorld());
+
+  const pause = () => world.stop();
+  const start = () => world.start();
+
+  const restart = () => {
+    world.stop();
+    const nextW = newWorld();
+
+    setWorld(nextW);
+  };
 
   const [speed, setSpeed] = useState<number>(world.simSpeed);
 
@@ -29,15 +41,40 @@ const App = observer((props: IProps) => {
       />
 
       <div className="controls">
-        <input 
-          type="number" 
-          value={speed} 
-          onChange={e => setSpeed(Number(e.target.value))} 
-        />
-        <button type="button" onClick={() => {world.simSpeed = speed}}>Setup</button>
+        <label className="flex-row form-block">
+          Speed
+          <input 
+            type="number" 
+            value={speed} 
+            onChange={e => setSpeed(Number(e.target.value))} 
+          />
+          <button type="button" onClick={() => {world.simSpeed = speed}}>Setup</button>
+        </label>
+        <button type="button" onClick={pause}>Pause</button>
+        <button type="button" onClick={start}>Start</button>
+        <button type="button" onClick={restart}>Restart</button>
       </div>
     </div>
   );
 });
 
 export default App;
+
+
+
+const newWorld = () : World => {
+  const world = new World();
+
+  const defaultGenome = new Genome(10000, 30, 40);
+  const cell = new AliveCell(world, ++World.lastCellId, new Point(4, 5), defaultGenome);
+
+  defaultGenome.instincts.push(InstinctRegistry.split);
+  defaultGenome.instincts.push(InstinctRegistry.actForInterest);
+  defaultGenome.instincts.push(getRandomMoveInstinct());
+
+  world.cells = [
+    cell
+  ];
+
+  return world;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -3,29 +3,10 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { World } from './model/world';
-import { Genome } from './model/life/genome';
-import { AliveCell } from './model/life/cells/aliveCell';
-import { InstinctRegistry } from './model/life/genome/instincts';
-import { Point } from './model/point';
 
 import './index.css';
-import { getRandomMoveInstinct } from './model/helpers';
 
-const world = new World();
-
-const defaultGenome = new Genome(10000, 30, 40);
-const cell = new AliveCell(world, ++World.lastCellId, new Point(4, 5), defaultGenome);
-
-defaultGenome.instincts.push(InstinctRegistry.split);
-defaultGenome.instincts.push(InstinctRegistry.actForInterest);
-defaultGenome.instincts.push(getRandomMoveInstinct());
-
-world.cells = [
-  cell
-];
-
-ReactDOM.render(<App world={world} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
